29719a813c632b07bf477158fef9bfb4f3be4af0
Robert Malloy <rmalloy1@usf.edu>
2023-06-22 19:11:40 -0400

    Pending Prompt Added to Main

--- MainMenu.py
+++ MainMenu.py
@@ -1,12 +1,15 @@
 from Pages import SkillsPage as Skills
+from Pages import LoginPage as login
 from Pages import ConnectionsPage as Connect
 from Pages import JobOpportunitiesPage as Job
+from Pages import ShowMyNetwork as Network
 from Pages.Useful import BrowseInCollegePage as Browse, DirectoriesPage as Directory, GeneralPage as General, BusinessSolutionsPage as Business
 from Pages.InCollegeImportant import AccessibilityPage as Accessibility,\
     BrandPolicyPage as Brand, CopyrightPage as Copyright, \
     UserAgreementPage as UserAgreement, CookiePolicyPage as Cookie, \
     CopyrightPolicyPage as CopyPolicy, About as About, \
     LanguagePage as Lang, PrivacyPage as Privacy
+from Util import db_helper as db
 
 
 class MainMenu:
@@ -14,6 +17,7 @@ class MainMenu:
         self.job_page = Job.JobOpportunitiesPage()
         self.connect_page = Connect.ConnectionsPage()
         self.skills_page = Skills.SkillsPage()
+        self.network = Network.ShowMyNetworkPage()
 
     def invalid_response(self, invalid_selection):
         print(f"{invalid_selection} is not a valid option. Please only enter the number")
@@ -30,6 +34,9 @@ class MainMenu:
             self.job_page.load_job_opportunities()
             return True
         elif user_choice == 14:
+            self.network.load_my_network()
+            return True
+        elif user_choice == 15:
             return False
         elif user_choice == 21:
             General.GeneralPage().menu()
@@ -75,6 +82,8 @@ class MainMenu:
             return True
 
     def main_menu_options(self):
+        if db.get_pending_from(login.username) is not None:
+            self.pending_friends_prompt()
         while True:
             self.print_menu()
             choice = int(input("\nPlease enter where you would like to navigate: "))
@@ -101,8 +110,8 @@ class MainMenu:
                 ["11.) Your Skill Development", "21.) General", "31.) Copyright Notice"],
                 ["12.) Your Connections", "22.) Browse InCollege", "32.) About"],
                 ["13.) Job/Internship Prospects", "23.) Business Solutions", "33.) Accessibility"],
-                ["14.) Exit", "24.) Directories", "34.) User Agreement"],
-                ["", "", "35.) Cookie Policy"],
+                ["14.) Show My Network", "24.) Directories", "34.) User Agreement"],
+                ["15.) Exit", "", "35.) Cookie Policy"],
                 ["", "", "36.) Copyright Policy"],
                 ["", "", "37.) Brand Policy"],
                 ["", "", "38.) Privacy Policy"],
@@ -110,3 +119,20 @@ class MainMenu:
         for row in menu:
             print("{:<31} | {:<31} | {:<31} ".format(*row))
         print("\n")
+
+    def pending_friends_prompt(self):
+        pending_friends = db.get_pending_from(login.username)
+        current_user = db.get_user(login.username)
+        for friend in pending_friends:
+            friend = db.get_user(friend)
+            choice = input(f"\n{friend[0]} has sent you a friend request. Would you like to accept? (Y/N)").lower()
+            if choice == "y":
+                print(f"{friend[0]} has been added")
+                db.add_friend(login.username, friend)
+                db.delete_pending(friend[0], current_user)
+            elif choice == "n":
+                print(f"{friend[0]} has been removed from your pending friend requests")
+                db.delete_pending(friend[0], current_user)
+            else:
+                print("Please select a valid option (Only enter a Y or N)")
+        return
---
+++ Pages/ShowMyNetwork.py
@@ -0,0 +1,4 @@
+class ShowMyNetworkPage:
+
+    def load_my_network(self):
+        print("user")
