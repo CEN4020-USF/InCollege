df8aed4e038d0556496d0fa9c4b8a7a1e545c0d8
Robert Malloy <rmalloy1@usf.edu>
2023-06-28 19:32:42 -0400

    db_helper updated for DB changes

--- Util/db_helper.py
+++ Util/db_helper.py
@@ -19,18 +19,18 @@ def db_close(conn, cursor):
     conn.close()
 
 
-def add_user(username, password, first_name, last_name, major="CS", uni="USF", email=1, sms=1, advert=1,
+def add_user(username, password, first_name, last_name, email=1, sms=1, advert=1,
              lang="English", sign_in=1, friends="{\"friends\": []}", pending_from="{\"friends\": []}",
              pending_to="{\"friends\": []}"):
     conn, cursor = db_connect()
 
     # Execute a query to insert data into the table
     insert_query = \
-        "INSERT INTO Users (Username, Password, first_name, last_name," \
-        " EmailEnabled, SMSEnabled, AdvertisingEnabled, Language, is_signed_in, Major, University," \
-        " Friends, PendingFrom, PendingTo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        "INSERT INTO tblUsers (Username, Password, FirstName, LastName," \
+        " EmailEnabled, SMSEnabled, AdvertisingEnabled, Language, SignedIn," \
+        " Friends, PendingFrom, PendingTo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
     values = (username, password, first_name, last_name, email, sms, advert, lang,
-              sign_in, major, uni, friends, pending_from, pending_to)
+              sign_in, friends, pending_from, pending_to)
     cursor.execute(insert_query, values)
 
     db_close(conn, cursor)
@@ -39,7 +39,7 @@ def add_user(username, password, first_name, last_name, major="CS", uni="USF", e
 def remove_user(username):
     conn, cursor = db_connect()
 
-    delete_query = "DELETE FROM Users WHERE username = ?"
+    delete_query = "DELETE FROM tblUsers WHERE username = ?"
     values = (username,)
     cursor.execute(delete_query, values)
 
@@ -49,7 +49,7 @@ def remove_user(username):
 def edit_user(username):
     conn, cursor = db_connect()
 
-    update_query = "UPDATE Users SET Username = ? WHERE Username = ?"
+    update_query = "UPDATE tblUsers SET Username = ? WHERE Username = ?"
     values = (username, username)
     cursor.execute(update_query, values)
 
@@ -59,7 +59,7 @@ def edit_user(username):
 def get_user(username):
     conn, cursor = db_connect()
 
-    select_query = "SELECT * FROM Users WHERE Username = ?"
+    select_query = "SELECT * FROM tblUsers WHERE Username = ?"
     values = (username,)
     cursor.execute(select_query, values)
 
@@ -73,7 +73,7 @@ def get_user(username):
 def check_name(firstname, last_name, is_mock=False):
     conn, cursor = db_connect()
 
-    select_query = "SELECT * FROM Users WHERE first_name = ?"
+    select_query = "SELECT * FROM tblUsers WHERE FirstName = ?"
     values = (firstname,)
     cursor.execute(select_query, values)
 
@@ -93,7 +93,7 @@ def check_name(firstname, last_name, is_mock=False):
 def get_first_name(firstname):
     conn, cursor = db_connect()
 
-    select_query = "SELECT * FROM Users WHERE first_name = ?"
+    select_query = "SELECT * FROM tblUsers WHERE FirstName = ?"
     values = (firstname,)
     cursor.execute(select_query, values)
 
@@ -107,7 +107,7 @@ def get_first_name(firstname):
 def get_last_name(lastname):
     conn, cursor = db_connect()
 
-    select_query = "SELECT Username FROM Users WHERE last_name = ?"
+    select_query = "SELECT Username FROM tblUsers WHERE LastName = ?"
     values = (lastname,)
     cursor.execute(select_query, values)
 
@@ -121,7 +121,7 @@ def get_last_name(lastname):
 def get_by_university(university):
     conn, cursor = db_connect()
 
-    select_query = "SELECT Username FROM Users WHERE University = ?"
+    select_query = "SELECT User FROM tblUserProfiles WHERE University = ?"
     values = (university,)
     cursor.execute(select_query, values)
 
@@ -135,7 +135,7 @@ def get_by_university(university):
 def get_by_major(major):
     conn, cursor = db_connect()
 
-    select_query = "SELECT Username FROM Users WHERE Major = ?"
+    select_query = "SELECT User FROM tblUserProfiles WHERE Major = ?"
     values = (major,)
     cursor.execute(select_query, values)
 
@@ -149,7 +149,7 @@ def get_by_major(major):
 def count_users():
     conn, cursor = db_connect()
 
-    count_query = "SELECT COUNT(*) FROM Users"
+    count_query = "SELECT COUNT(*) FROM tblUsers"
     cursor.execute(count_query)
 
     count = cursor.fetchone()[0]
@@ -159,12 +159,13 @@ def count_users():
     return count
 
 
-def add_job(title, description, employer, location, salary, created_by):
+def add_job( created_by, title, description, employer, location, salary, date_started="", date_ended=""):
     conn, cursor = db_connect()
 
     # Execute a query to insert data into the table
-    insert_query = "INSERT INTO Jobs (Title, Description, Employer, Location, Salary, Created_By) VALUES (?, ?, ?, ?, ?, ?)"
-    values = (title, description, employer, location, salary, created_by)
+    insert_query = "INSERT INTO tblJobs (Title, Description, Employer, Location, Salary, CreatedBy, DateStarted," \
+                   " DateEnded) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+    values = (title, description, employer, location, salary, created_by, date_started, date_ended)
     cursor.execute(insert_query, values)
 
     db_close(conn, cursor)
@@ -173,7 +174,7 @@ def add_job(title, description, employer, location, salary, created_by):
 def remove_job(job_id):
     conn, cursor = db_connect()
 
-    delete_query = "DELETE FROM Jobs WHERE id = ?"
+    delete_query = "DELETE FROM tblJobs WHERE id = ?"
     values = (job_id,)
     cursor.execute(delete_query, values)
 
@@ -183,31 +184,31 @@ def remove_job(job_id):
 def edit_job(job_id, company, position, salary):
     conn, cursor = db_connect()
 
-    update_query = "UPDATE Jobs SET Company = ?, Position = ?, Salary = ? WHERE id = ?"
+    update_query = "UPDATE tblJobs SET Company = ?, Position = ?, Salary = ? WHERE id = ?"
     values = (company, position, salary, job_id)
     cursor.execute(update_query, values)
 
     db_close(conn, cursor)
 
 
-def get_job(job_id):
+def get_job_by_user(created_by):
     conn, cursor = db_connect()
 
-    select_query = "SELECT * FROM Jobs WHERE id = ?"
-    values = (job_id,)
-    cursor.execute(select_query, values)
+    update_query = f"SELECT * FROM tblJobs WHERE Salary IS NULL AND CreatedBy = ? LIMIT 3"
+    values = (created_by,)
+    cursor.execute(update_query, values)
 
-    job = cursor.fetchone()
+    jobs = cursor.fetchall()
 
     db_close(conn, cursor)
 
-    return job
+    return jobs
 
 
 def count_jobs():
     conn, cursor = db_connect()
 
-    count_query = "SELECT COUNT(*) FROM Jobs"
+    count_query = "SELECT COUNT(*) FROM tblJobs"
     cursor.execute(count_query)
 
     count = cursor.fetchone()[0]
@@ -220,7 +221,7 @@ def count_jobs():
 def toggle_sms(username, toggle):
     conn, cursor = db_connect()
 
-    update_query = "UPDATE Users SET SMSEnabled = ? WHERE Username = ?"
+    update_query = "UPDATE tblUsers SET SMSEnabled = ? WHERE Username = ?"
     values = (toggle, username)
     cursor.execute(update_query, values)
 
@@ -230,7 +231,7 @@ def toggle_sms(username, toggle):
 def toggle_email(username, toggle):
     conn, cursor = db_connect()
 
-    update_query = "UPDATE Users SET EmailEnabled = ? WHERE Username = ?"
+    update_query = "UPDATE tblUsers SET EmailEnabled = ? WHERE Username = ?"
     values = (toggle, username)
     cursor.execute(update_query, values)
 
@@ -240,7 +241,7 @@ def toggle_email(username, toggle):
 def toggle_advertising(username, toggle):
     conn, cursor = db_connect()
 
-    update_query = "UPDATE Users SET AdvertisingEnabled = ? WHERE Username = ?"
+    update_query = "UPDATE tblUsers SET AdvertisingEnabled = ? WHERE Username = ?"
     values = (toggle, username)
     cursor.execute(update_query, values)
 
@@ -250,7 +251,7 @@ def toggle_advertising(username, toggle):
 def change_language(username, language):
     conn, cursor = db_connect()
 
-    update_query = "UPDATE Users SET Language = ? WHERE Username = ?"
+    update_query = "UPDATE tblUsers SET Language = ? WHERE Username = ?"
     values = (language, username)
     cursor.execute(update_query, values)
 
@@ -260,17 +261,17 @@ def change_language(username, language):
 def user_signed_in(username):
     conn, cursor = db_connect()
 
-    update_query = "UPDATE Users SET is_signed_in = 1 WHERE Username = ?"
+    update_query = "UPDATE tblUsers SET SignedIn = 1 WHERE Username = ?"
     values = (username,)
     cursor.execute(update_query, values)
 
     db_close(conn, cursor)
 
-#sets all users in the data base to be signed out
+#sets all tblUsers in the data base to be signed out
 def sign_out_all():
     conn, cursor = db_connect()
 
-    cursor.execute("UPDATE Users SET is_signed_in = 0")
+    cursor.execute("UPDATE tblUsers SET SignedIn = 0")
     conn.commit()
 
     db_close(conn, cursor)
@@ -280,7 +281,7 @@ def is_user_signed_in():
     conn, cursor = db_connect()
     flag = False
 
-    cursor.execute("SELECT COUNT(*) FROM Users WHERE is_signed_in = 1")
+    cursor.execute("SELECT COUNT(*) FROM tblUsers WHERE SignedIn = 1")
     result = cursor.fetchone()[0]
 
     if result > 0:
@@ -293,7 +294,7 @@ def is_user_signed_in():
 def add_friend(current_user, target_user):
     conn, cursor = db_connect()
 
-    select_query = "SELECT Friends FROM Users WHERE Username=?"
+    select_query = "SELECT Friends FROM tblUsers WHERE Username=?"
     values = (current_user,)
     cursor.execute(select_query, values)
     current_user_friends = cursor.fetchone()
@@ -307,13 +308,13 @@ def add_friend(current_user, target_user):
         current_friends_json = json.loads(current_friends_json_str)
         current_friends_json['friends'].append(target_user[0])
         current_friends_json = json.dumps(current_friends_json)
-        cursor.execute("UPDATE Users SET Friends=? WHERE Username=?", (current_friends_json, current_user))
+        cursor.execute("UPDATE tblUsers SET Friends=? WHERE Username=?", (current_friends_json, current_user))
 
         target_friends_json_str = target_user_friends[0]
         target_friends_json = json.loads(target_friends_json_str)
         target_friends_json['friends'].append(current_user)
         target_friends_json = json.dumps(target_friends_json)
-        cursor.execute("UPDATE Users SET Friends=? WHERE Username=?", (target_friends_json, target_user[0]))
+        cursor.execute("UPDATE tblUsers SET Friends=? WHERE Username=?", (target_friends_json, target_user[0]))
 
         conn.commit()
     db_close(conn, cursor)
@@ -322,7 +323,7 @@ def add_friend(current_user, target_user):
 def delete_friend(current_user, target_user):
     conn, cursor = db_connect()
 
-    select_query = "SELECT Friends FROM Users WHERE Username=?"
+    select_query = "SELECT Friends FROM tblUsers WHERE Username=?"
     values = (current_user,)
     cursor.execute(select_query, values)
     current_user_friends = cursor.fetchone()
@@ -336,13 +337,13 @@ def delete_friend(current_user, target_user):
         if target_user[0] in friends_json['friends']:
             friends_json['friends'].remove(target_user[0])
             friends_json_str = json.dumps(friends_json)
-            cursor.execute("UPDATE Users SET Friends=? WHERE Username=?", (friends_json_str, current_user))
+            cursor.execute("UPDATE tblUsers SET Friends=? WHERE Username=?", (friends_json_str, current_user))
 
         friends_json = json.loads(target_user_friends[0])
         if current_user in friends_json['friends']:
             friends_json['friends'].remove(current_user)
             friends_json_str = json.dumps(friends_json)
-            cursor.execute("UPDATE Users SET Friends=? WHERE Username=?", (friends_json_str, target_user[0]))
+            cursor.execute("UPDATE tblUsers SET Friends=? WHERE Username=?", (friends_json_str, target_user[0]))
 
         conn.commit()
 
@@ -352,12 +353,12 @@ def delete_friend(current_user, target_user):
 def add_pending(current_user, target_user):
     conn, cursor = db_connect()
 
-    select_query = "SELECT PendingTo FROM Users WHERE Username=?"
+    select_query = "SELECT PendingTo FROM tblUsers WHERE Username=?"
     values = (current_user,)
     cursor.execute(select_query, values)
     current_user_friends = cursor.fetchone()
 
-    select_query = "SELECT PendingFrom FROM Users WHERE Username=?"
+    select_query = "SELECT PendingFrom FROM tblUsers WHERE Username=?"
     values = (target_user[0],)
     cursor.execute(select_query, values)
     target_user_friends = cursor.fetchone()
@@ -367,12 +368,12 @@ def add_pending(current_user, target_user):
         current_friends_json = json.loads(current_friends_json_str)
         current_friends_json['friends'].append(target_user[0])
         current_friends_json = json.dumps(current_friends_json)
-        cursor.execute("UPDATE Users SET PendingTo=? WHERE Username=?", (current_friends_json, current_user))
+        cursor.execute("UPDATE tblUsers SET PendingTo=? WHERE Username=?", (current_friends_json, current_user))
         target_friends_json_str = target_user_friends[0]
         target_friends_json = json.loads(target_friends_json_str)
         target_friends_json['friends'].append(current_user)
         target_friends_json = json.dumps(target_friends_json)
-        cursor.execute("UPDATE Users SET PendingFrom=? WHERE Username=?", (target_friends_json, target_user[0]))
+        cursor.execute("UPDATE tblUsers SET PendingFrom=? WHERE Username=?", (target_friends_json, target_user[0]))
 
         conn.commit()
     db_close(conn, cursor)
@@ -381,12 +382,12 @@ def add_pending(current_user, target_user):
 def delete_pending(current_user, target_user):
     conn, cursor = db_connect()
 
-    select_query = "SELECT PendingTo FROM Users WHERE Username=?"
+    select_query = "SELECT PendingTo FROM tblUsers WHERE Username=?"
     values = (current_user,)
     cursor.execute(select_query, values)
     current_user_friends = cursor.fetchone()
 
-    select_query = "SELECT PendingFrom FROM Users WHERE Username=?"
+    select_query = "SELECT PendingFrom FROM tblUsers WHERE Username=?"
     values = (target_user[0],)
     cursor.execute(select_query, values)
     target_user_friends = cursor.fetchone()
@@ -397,13 +398,13 @@ def delete_pending(current_user, target_user):
         if target_user[0] in current_friends_json['friends']:
             current_friends_json['friends'].remove(target_user[0])
             current_friends_json = json.dumps(current_friends_json)
-            cursor.execute("UPDATE Users SET PendingTo=? WHERE Username=?", (current_friends_json, current_user))
+            cursor.execute("UPDATE tblUsers SET PendingTo=? WHERE Username=?", (current_friends_json, current_user))
         target_friends_json_str = target_user_friends[0]
         target_friends_json = json.loads(target_friends_json_str)
         if current_user in target_friends_json['friends']:
             target_friends_json['friends'].remove(current_user)
             target_friends_json = json.dumps(target_friends_json)
-            cursor.execute("UPDATE Users SET PendingFrom=? WHERE Username=?", (target_friends_json, target_user[0]))
+            cursor.execute("UPDATE tblUsers SET PendingFrom=? WHERE Username=?", (target_friends_json, target_user[0]))
 
         conn.commit()
     db_close(conn, cursor)
@@ -412,7 +413,7 @@ def delete_pending(current_user, target_user):
 def get_friends(username):
     conn, cursor = db_connect()
 
-    select_query = "SELECT Friends FROM Users WHERE Username = ?"
+    select_query = "SELECT Friends FROM tblUsers WHERE Username = ?"
     values = (username,)
     cursor.execute(select_query, values)
 
@@ -431,7 +432,7 @@ def get_friends(username):
 def get_pending_to(username):
     conn, cursor = db_connect()
 
-    select_query = "SELECT PendingTo FROM Users WHERE Username = ?"
+    select_query = "SELECT PendingTo FROM tblUsers WHERE Username = ?"
     values = (username,)
     cursor.execute(select_query, values)
 
@@ -450,7 +451,7 @@ def get_pending_to(username):
 def get_pending_from(username):
     conn, cursor = db_connect()
 
-    select_query = "SELECT PendingFrom FROM Users WHERE Username = ?"
+    select_query = "SELECT PendingFrom FROM tblUsers WHERE Username = ?"
     values = (username,)
     cursor.execute(select_query, values)
 
@@ -465,5 +466,71 @@ def get_pending_from(username):
     db_close(conn, cursor)
     return friends_list
 
+
+def add_user_profile(username, title=None, major=None, uni=None, about=None, is_created=0):
+    conn, cursor = db_connect()
+
+    update_query = "INSERT INTO tblUserProfiles (Title, Major, University, About, IsCreated, User) VALUES (?, ?, ?, ?, ?, ?)"
+    values = (title, major, uni, about, is_created, username)
+    cursor.execute(update_query, values)
+
+    db_close(conn, cursor)
+
+
+def get_user_profile(username):
+    conn, cursor = db_connect()
+
+    update_query = f"SELECT * FROM tblUserProfiles WHERE User = ?"
+    values = (username,)
+    cursor.execute(update_query, values)
+
+    profile = cursor.fetchone()
+
+    db_close(conn, cursor)
+
+    return profile
+
+def set_user_profile(username, column, update):
+    conn, cursor = db_connect()
+
+    update_query = f"UPDATE tblUserProfiles SET {column} = ? WHERE User = ?"
+    values = (update, username,)
+    cursor.execute(update_query, values)
+
+    db_close(conn, cursor)
+
+
+def add_education(username, school, degree, start, end):
+    conn, cursor = db_connect()
+
+    update_query = "INSERT INTO tblEducation (School, Degree, StartYear, EndYear, CreatedBy) VALUES (?, ?, ?, ?, ?)"
+    values = (school, degree, start, end, username)
+    cursor.execute(update_query, values)
+
+    db_close(conn, cursor)
+
+
+def get_education(username):
+    conn, cursor = db_connect()
+
+    update_query = f"SELECT * FROM tblEducation WHERE CreatedBy = ?"
+    values = (username,)
+    cursor.execute(update_query, values)
+
+    education = cursor.fetchall()
+
+    db_close(conn, cursor)
+
+    return education
+
+
+def delete_education(username):
+    conn, cursor = db_connect()
+
+    delete_query = "DELETE FROM tblEducation WHERE CreatedBy = ?"
+    values = (username,)
+    cursor.execute(delete_query, values)
+
+    db_close(conn, cursor)
 
 
