1adc441929657d77c91019a40c51414a6b3ecfcb
Robert Malloy <rmalloy1@usf.edu>
2023-06-28 19:35:34 -0400

    Update ConnectionsPage for DB updates

--- Pages/ConnectionsPage.py
+++ Pages/ConnectionsPage.py
@@ -35,7 +35,7 @@ class ConnectionsPage:
 
     def find_users_by_last_name(self):
         last_name = input("Please Enter Last Name: ")
-        users = db.get_last_name(last_name.lower())
+        users = db.get_last_name(last_name.title())
         if len(users) == 0:
             print(f"There were no users found with the last name, {last_name}")
             return
@@ -44,7 +44,7 @@ class ConnectionsPage:
 
     def find_users_by_major(self):
         major = input("Please Enter Major: ")
-        users = db.get_by_major(major.lower())
+        users = db.get_by_major(major.title())
         if len(users) == 0:
             print(f"There were no users found majoring in {major}")
             return
@@ -53,7 +53,7 @@ class ConnectionsPage:
 
     def find_users_by_university(self):
         university = input("Please Enter University: ")
-        users = db.get_by_university(university.lower())
+        users = db.get_by_university(university.title())
         if len(users) == 0:
             print(f"There were no users found at {university}")
             return
@@ -86,7 +86,8 @@ class ConnectionsPage:
                 ["=" * column_width, "=" * column_width, "=" * column_width, "=" * column_width, "=" * column_width, ]]
         for user in users:
             user = db.get_user(user)
-            user_attributes = [user[0], user[2], user[3], user[10], user[9]]
+            profile = db.get_user_profile(user[0])
+            user_attributes = [user[0], user[2], user[3], profile[2], profile[1]]
             menu.append(user_attributes)
         for row in menu:
             print("{:<25} | {:<25} | {:<25} | {:<25} | {:<25}".format(*row))
