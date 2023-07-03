e3cb0ea81203dcd6fb93a626ad54a0797d56ccf5
Christopher Luevano <luevano@usf.edu>
2023-07-02 20:50:14 -0400

    ben's fix

--- Pages/Useful/GeneralPage.py
+++ Pages/Useful/GeneralPage.py
@@ -19,45 +19,45 @@ class GeneralPage:
         print("7.) Developers")
         print("0.) Return To Menu")
 
-        choice = int(input("Enter Option: "))
+        choice = input("Enter Option: ")
 
-
-        if choice == 1:
+        if choice == "1":
             if db.is_user_signed_in():
                 print("\n**It seems you are already signed in.**")
                 return self.menu()
             print("\n1.) Sign In")
             print("2.) Create Account")
             print("0.) Return to previous page")
-            option = int(input("Enter option: "))
+            option = input("Enter option: ")
             while True:
-                if option != 1 and option !=2 and option != 0:
-                    option = int(input("Invalid input please enter 1, 2, or 0: "))
+                if option != "1" and option != "2" and option != "0":
+                    option = input("Invalid input please enter 1, 2, or 0: ")
                 else:
                     break
-            if option == 1:
+            if option == "1":
                 LoginPage.Login().sign_in()
-            elif option == 2:
+            elif option == "2":
                 LoginPage.Login().create_account()
-            elif option == 0:
+            elif option == "0":
                 GeneralPage().menu()
-                
 
-        elif choice == 2:
+        elif choice == "2":
             Help.HelpCenterPage().menu()
-        elif choice == 3:
+        elif choice == "3":
             About.AboutPage().menu()
-        elif choice == 4:
+        elif choice == "4":
             Press.PressPage().menu()
-        elif choice == 5:
+        elif choice == "5":
             Blog.BlogPage().menu()
-        elif choice == 6:
+        elif choice == "6":
             Career.CareersPage().menu()
-        elif choice == 7:
+        elif choice == "7":
             Developer.DeveloperPage().menu()
-        elif choice == 0:
-            return
-
+        elif choice == "0":
+            if not db.is_user_signed_in():
+                LoginPage.Login().menu()
+            else:
+                return
         else:
+            print(f"{choice} is not supported. Please try again")
             self.menu()
-
